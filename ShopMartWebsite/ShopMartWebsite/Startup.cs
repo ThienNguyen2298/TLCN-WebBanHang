8e73c376abefcbdb29d5f749c38d19371263d570
huuThien <thiennguyen.it.ute@gmail.com>
2019-11-10 18:01:57 +0700

    login-admin

--- ShopMartWebsite/ShopMartWebsite/Startup.cs
+++ ShopMartWebsite/ShopMartWebsite/Startup.cs
@@ -8,9 +8,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ShopMartWebsite.Data;
-using ShopMartWebsite.Models;
+
+
 using ShopMartWebsite.Services;
+using ShopMartWebsite.Data;
+using ShopMartWebsite.Entities;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using ShopMartWebsite.Extensions;
+using ShopMartWebsite.Helpers;
+using Microsoft.AspNetCore.Http;
 
 namespace ShopMartWebsite
 {
@@ -26,22 +33,28 @@ namespace ShopMartWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ShopDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("FirstConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<ShopDbContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddAuthentication().AddCookie();
+            //services.AddScoped<IUserClaimsPrincipalFactory<User>, CustomClaimsPrincipalFactory>();
+            //services.AddScoped<IUserClaimsPrincipalFactory<User>, CustomClaimsPrincipalFactory>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            ILoggerFactory loggerFactory
+            )
         {
+            loggerFactory.AddFile("Logs/shop-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -56,13 +69,19 @@ namespace ShopMartWebsite
             app.UseStaticFiles();
 
             app.UseAuthentication();
+            
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "Admin",
+                    template: "{area=exists}/{controller=Login}/{action=Index}/{id?}"
+                    );
             });
+            
         }
     }
 }
